eea99e25c2190add61d894f8117bc6f0d2c165e6
agent2vinod <agent2vinod@gmail.com>
2021-10-02 16:30:55 +0530

    first commit

--- src/main/java/com/example/springbootgenkins/SpringbootgenkinsApplication.java
+++ src/main/java/com/example/springbootgenkins/SpringbootgenkinsApplication.java
@@ -1,15 +1,27 @@
 package com.example.springbootgenkins;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import java.util.logging.Logger;
+
 @SpringBootApplication
 @RestController
 public class SpringbootgenkinsApplication {
 
+    public static Logger logger = (Logger) LoggerFactory.getLogger(SpringbootgenkinsApplication.class);
+
+    @PostConstruct
+    public void init(){
+        logger.info("Application Started !!");
+    }
+
     public static void main(String[] args) {
+        logger.info("Application Executed !!");
         SpringApplication.run(SpringbootgenkinsApplication.class, args);
     }
 
